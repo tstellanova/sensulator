63cbf74f923857749f3e51eec301f9d7ca37e887
Todd Stellanova <todd.stellanova@formation.ai>
2019-03-24 09:44:40 -0700

    Move example to runnable main.rs

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// Copyright 2019, Todd Stellanova
+// License: see LICENSE file
+
+use sensulator::Sensulator;
+
+/// Latitude of Berkeley, California
+const HOME_LAT:f32 = 37.8716;
+/// Absolute error of a typical GPS sensor (degrees)
+const GPS_HORIZ_ABS_ERROR:f32 = 2e-6;
+/// Relative error of a typical GPS sensor (degrees)
+const GPS_HORIZ_REL_ERROR:f32 = 4.5e-5;
+
+fn main() {
+  let mut fake_gps_lat = Sensulator::new(HOME_LAT, GPS_HORIZ_ABS_ERROR, GPS_HORIZ_REL_ERROR);
+  loop {
+    // update the sensor reading and display (requires a mutable sensulator reference)
+    println!("new lat: {}", fake_gps_lat.measure());
+    // simply display the last measured value (may use an immutable reference)
+    println!("old lat: {}", fake_gps_lat.peek());
+  }
+}
+
